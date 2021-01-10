2b0cd1f5a1211ee191b0b1225e11c4186242f1de
kawasaki2254 <kawasakitota2254@gmail.com>
2021-01-10 12:12:29 +0900

    問題31完了

--- リスト型/No.31.py
+++ リスト型/No.31.py
@@ -5,3 +5,11 @@
 
 l = ['Python', 'Ruby', 'PHP', 'JavaScript']
 
+min_length_word, min_length = l[0], len(l[0])
+
+for word in l:
+    if len(word) < min_length:
+        min_length_word = word
+        # min_length = len(word)
+
+print(f'一番短い単語 : {min_length_word}')
